76b1f28fa85112a2e37975ba359bd81d36f41923
RaidenGay <euauangklu.2234@gmail.com>
2024-09-26 10:33:11 +0700

    Lab13

---
+++ Minilab1/Assets/Samples/KS Physics/0.0.8/Configurable Joint Samples/Scenes/Scripts/CraneController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+
+public class CraneController : MonoBehaviour
+{
+    [SerializeField] protected ConfigurableJoint craneMovingPartJoint;
+
+    
+    
+    [SerializeField] protected Slider sliderX;
+    [SerializeField] protected Slider sliderY;
+    [SerializeField] protected Slider sliderZ;
+    
+    private Vector3 targetPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderX.onValueChanged.AddListener(SliderXChange);
+        sliderY.onValueChanged.AddListener(SliderYChange);
+        sliderZ.onValueChanged.AddListener(SliderZChange);
+
+        targetPosition = craneMovingPartJoint.targetPosition;
+    }
+    
+    void SliderXChange(float val)
+    {
+        targetPosition.x = val;
+        craneMovingPartJoint.targetPosition = targetPosition;
+    }
+    void SliderYChange(float val)
+    {
+        targetPosition.y = val;
+        craneMovingPartJoint.targetPosition = targetPosition;
+    }
+    void SliderZChange(float val)
+    {
+        targetPosition.z = val;
+        craneMovingPartJoint.targetPosition = targetPosition;
+    }
+}
---
+++ Minilab1/Assets/Samples/KS Physics/0.0.8/Configurable Joint Samples/Scenes/Scripts/CraneReleaseObjectMechanic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneReleaseObjectMechanic : MonoBehaviour
+{
+    [SerializeField] private FixedJoint fixedJoint;
+
+
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //fixedJoint.connectedBody = null;
+            Destroy(fixedJoint);
+        }
+    }
+    
+    
+}
